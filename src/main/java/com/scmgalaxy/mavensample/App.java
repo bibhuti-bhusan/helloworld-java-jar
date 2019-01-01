da67a3244e9d2bfb515c2df6cd37675b8e2c8d55
Bibhuti bhusan samantaray <41391175+bibhuti-bhusan@users.noreply.github.com>
2019-01-01 23:59:05 +0530

    MPHA-2 FIXED
    MPHA-2 FIXED

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-2--d" );
+        System.out.println( "Hello World RefX! MPHA-2 FIXED MAH-2--d" );
     }
 }
